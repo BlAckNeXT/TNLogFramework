4d2b1b7ce3e171bc4e159e5ed8b69a021e9324d1
zhangxuejian <zhangxuejian@syswin.com>
2016-01-08 09:20:29 +0800

    TNLogFramework

---
+++ Products/TNLogFramework.framework/Headers/TNLogFramework.h
@@ -0,0 +1,20 @@
+//
+//  TNLogFramework.h
+//  TNLogFramework
+//
+//  Created by max on 15/7/29.
+//  Copyright (c) 2015年 max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TNLog.h"
+
+//! Project version number for TNLogFramework.
+FOUNDATION_EXPORT double TNLogFrameworkVersionNumber;
+
+//! Project version string for TNLogFramework.
+FOUNDATION_EXPORT const unsigned char TNLogFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TNLogFramework/PublicHeader.h>
+
+
